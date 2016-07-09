cdf79d548c1c24654fae6773ae356776e7dd6b76
Thomas Kirven <thomasckirven@gmail.com>
2016-07-09 17:26:29 -0400

    Update I2Cdev.cpp

--- I2CDev/I2Cdev.cpp
+++ I2CDev/I2Cdev.cpp
@@ -54,7 +54,7 @@ THE SOFTWARE.
 #include <linux/i2c-dev.h>
 #include "I2Cdev.h"
 
-i2c_path=std::"/" + string("dev") + "/" + string("i2c-1");
+std::string i2c_path = std::"/" + string("dev") + "/" + string("i2c-1");
 
 /** Default constructor.
  */
